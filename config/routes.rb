e692b6dc36dcb6d7f466ab2cd1ff139628d4361b
Sara Canfield <slcanfield1@verizon.net>
2015-11-04 15:59:45 -0500

    added static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,54 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
+  root "home#index"
+  get '/'  => "home#index"
+    
+    # get "users/sign_in" => "sessions#new"
+    # post  "users/sign_in" => "sessions#create"
+    # get "users/sign_out" => "sessions#destroy"
+
+    resources :users do
+      resources :cards
+    end
+
+    resources :cardusers
+
+#             Prefix Verb   URI Pattern                              Controller#Action
+#           root GET    /                                        home#index
+#                GET    /                                        home#index
+#  users_sign_in GET    /users/sign_in(.:format)                 sessions#new
+#                POST   /users/sign_in(.:format)                 sessions#create
+# users_sign_out GET    /users/sign_out(.:format)                sessions#destroy
+#     user_cards GET    /users/:user_id/cards(.:format)          cards#index
+#                POST   /users/:user_id/cards(.:format)          cards#create
+#  new_user_card GET    /users/:user_id/cards/new(.:format)      cards#new
+# edit_user_card GET    /users/:user_id/cards/:id/edit(.:format) cards#edit
+#      user_card GET    /users/:user_id/cards/:id(.:format)      cards#show
+#                PATCH  /users/:user_id/cards/:id(.:format)      cards#update
+#                PUT    /users/:user_id/cards/:id(.:format)      cards#update
+#                DELETE /users/:user_id/cards/:id(.:format)      cards#destroy
+#          users GET    /users(.:format)                         users#index
+#                POST   /users(.:format)                         users#create
+#       new_user GET    /users/new(.:format)                     users#new
+#      edit_user GET    /users/:id/edit(.:format)                users#edit
+#           user GET    /users/:id(.:format)                     users#show
+#                PATCH  /users/:id(.:format)                     users#update
+#                PUT    /users/:id(.:format)                     users#update
+#                DELETE /users/:id(.:format)                     users#destroy
+#      cardusers GET    /cardusers(.:format)                     cardusers#index
+#                POST   /cardusers(.:format)                     cardusers#create
+#   new_carduser GET    /cardusers/new(.:format)                 cardusers#new
+#  edit_carduser GET    /cardusers/:id/edit(.:format)            cardusers#edit
+#       carduser GET    /cardusers/:id(.:format)                 cardusers#show
+#                PATCH  /cardusers/:id(.:format)                 cardusers#update
+#                PUT    /cardusers/:id(.:format)                 cardusers#update
+#                DELETE /cardusers/:id(.:format)                 cardusers#destroy
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
