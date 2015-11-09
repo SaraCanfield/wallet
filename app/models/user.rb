81882bf8892351f27ebc63e78177fec609403413
Sara Canfield <slcanfield1@verizon.net>
2015-11-09 16:40:04 -0500

    added password stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+	before_save { self.email = email.downcase }
+	# validates :phone_number, format: { with: /\d{3}-\d{3}-\d{4}/, message: "bad format" }
+	validates :lname,  presence: true, length: { maximum: 50 }
+	validates :fname,  presence: true, length: { maximum: 50 }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+	validates :password, length: { minimum: 4 }
+	has_secure_password	
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151104201546) do
+ActiveRecord::Schema.define(version: 20151109002602) do
 
   create_table "cards", force: :cascade do |t|
     t.integer  "card_number"
@@ -38,8 +38,11 @@ ActiveRecord::Schema.define(version: 20151104201546) do
     t.string   "fname"
     t.string   "lname"
     t.boolean  "admin"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
