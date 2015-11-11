ab47337249784a09f0c21a440af450263ea60353
Sara Canfield <slcanfield1@verizon.net>
2015-11-11 17:14:32 -0500

    added signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
-  def new
-  end
+	  def new
+	  	 @user = User.new
+	  end
+
+	  def show
+	  	@user = User.find(params[:id])
+	  end
+
+	  def create
+		    @user = User.new(user_params)
+		    if @user.save
+		    	#flash[:success] = "Welcome to MyWallet!"￼
+		    	redirect_to root
+		    else
+		    render 'new'
+		    end
+	   end 
+
+private
+
+  def user_params
+    params.require(:user).permit(:fname, :lname, :email, :password, :password_confirmation)
+   end
+
 end
